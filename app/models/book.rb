ba065a682628265057cfec7534e4f290be0ab14b
Brittany Owen <owen.brittany@gmail.com>
2021-03-31 14:50:55 -0500

    seed data added and database successfully seeded

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,7 +2,7 @@ class Book < ApplicationRecord
   validates :title, presence: true
   validates :author, presence: true
   validates :image_url, presence: true
-  validates :summary, presence: true, length: {maximum: 500}
+  validates :summary, presence: true, length: {maximum: 700}
   belongs_to :user
   has_many :reviews
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,5 @@
 class Review < ApplicationRecord
-  validates :content, length: {maximum: 250}
+  validates :content, length: {maximum: 500}
   belongs_to :user
   belongs_to :book
 end
