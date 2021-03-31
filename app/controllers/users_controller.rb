3805d420ccbc86cc1a79c0393dc655c7e35c647f
Brittany Owen <owen.brittany@gmail.com>
2021-03-31 13:53:44 -0500

    authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,16 @@
 class UsersController < ApplicationController
-
+  before_action :authorize_request, except: :create
+  
   # POST /users
   def create
     @user = User.new(user_params)
 
     if @user.save
-      render json: @user, status: :created, location: @user
+      @token = encode({id: @user.id})
+      render json: {
+        user: @user.attributes.except("password_digest"), 
+        token: @token
+        }, status: :created, location: @user
     else
       render json: @user.errors, status: :unprocessable_entity
     end
