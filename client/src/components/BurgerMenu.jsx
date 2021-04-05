2054c643c15b7382a5ee40406d96ff0cc77ee52a
Brittany Owen <owen.brittany@gmail.com>
2021-04-05 14:29:37 -0500

    passed props

--- client/src/components/BurgerMenu.jsx
+++ client/src/components/BurgerMenu.jsx
@@ -1,21 +1,34 @@
-import React, { useState } from 'react';
-import { NavLink } from "react-router-dom";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 //hamburger menu tutorial https://www.youtube.com/watch?v=XQCuew98W4k&feature=emb_title
 
-
-
-
-
-
 function BurgerMenu(props) {
-  const [click, setClick] = useState(false)
-  const handleClick = () => setClick(!click)
+  const { currentUser, handleLogout } = props;
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
   return (
-    <div>
-      BurgerMenu
-    </div>
+    <>
+      <div className="nav-options">
+        {currentUser ? (
+          <>
+            <p>Hi, {currentUser.username}!</p>
+            <div className="options">
+              <Link to="/books/add">Add Book</Link>
+              <button className="logout" onClick={handleLogout}>
+                Logout
+              </button>
+            </div>
+          </>
+        ) : (
+          <>
+            <Link to="/login">Log In</Link>
+            <Link to="/signup">Sign Up</Link>
+          </>
+        )}
+      </div>
+    </>
   );
 }
 
--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,42 +1,52 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import {useMediaPredicate} from "react-media-hook"
-import Footer from "../components/Footer"
-import BurgerMenu from "../components/BurgerMenu"
+import { useMediaPredicate } from "react-media-hook";
+import Footer from "../components/Footer";
+import BurgerMenu from "../components/BurgerMenu";
 
 function Nav(props) {
   const { currentUser, handleLogout } = props;
-  const mediaQuery = useMediaPredicate("(max-width:500px)")
+  const mediaQuery = useMediaPredicate("(max-width:500px)");
   return (
     <div className="nav">
       <div className="nav-details">
         <div className="tbn-logo">
-        <Link to="/">
-          <img className="logo" src="https://imgur.com/qeA3rui.png" alt="book spine icon"/>
-          <p className="app-name">The<br></br>Book<br></br>Nook</p>
-      </Link>
+          <Link to="/">
+            <img
+              className="logo"
+              src="https://imgur.com/qeA3rui.png"
+              alt="book spine icon"
+            />
+            <p className="app-name">
+              The<br></br>Book<br></br>Nook
+            </p>
+          </Link>
         </div>
-        {mediaQuery ? (<BurgerMenu />): (
+        {mediaQuery ? (
+          <BurgerMenu currentUser={currentUser} handleLogout={handleLogout} />
+        ) : (
           <div className="nav-options">
-          {currentUser ? (
-            <>
-            <p>Hi, {currentUser.username}!</p>
-            <div className="options">
-            <Link to="/books/add">Add Book</Link>
-            <button className="logout" onClick={handleLogout}>Logout</button>
-            </div>
-            </>
+            {currentUser ? (
+              <>
+                <p>Hi, {currentUser.username}!</p>
+                <div className="options">
+                  <Link to="/books/add">Add Book</Link>
+                  <button className="logout" onClick={handleLogout}>
+                    Logout
+                  </button>
+                </div>
+              </>
             ) : (
               <>
-              <Link to="/login">Log In</Link>
-              <Link to="/signup">Sign Up</Link>
+                <Link to="/login">Log In</Link>
+                <Link to="/signup">Sign Up</Link>
               </>
-              )}
-              </div>
-              )}
-              </div>
+            )}
+          </div>
+        )}
+      </div>
       <div className="footer1">
-      <Footer />
+        <Footer />
       </div>
     </div>
   );
